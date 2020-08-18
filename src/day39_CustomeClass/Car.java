8831d5f243a7f996a70ff1615c1695c46298cb0b
Olga Andratova <orgaeva00@gmail.com>
2020-08-18 19:42:35 -0400

    day43 class

--- src/day39_CustomeClass/Car.java
+++ src/day39_CustomeClass/Car.java
@@ -1,6 +1,10 @@
 package day39_CustomeClass;
 
 public class Car {
+    /*
+    Attributes:
+    model, brand, year, color, mileadge, price
+     */
 
     String brand;
     String model;
@@ -9,8 +13,27 @@ public class Car {
     double mileage;
     double price;
 
+    public void setCarInfo(String carBrand, String carModel, int carYear, String carColor, double carMileage, double carPrice){
+
+    }
+    // sets the info of the car objects
+
+    // 2012 Toyota Corolla,Red,10000,$ 45000
+    public void getCarInfo() {
+        System.out.println(year + " " + brand + " " + model + ", " + color + ", " + mileage + ", $" + price);
+
+    }
 
 
 
 
+/*
+car1: Toyota
+car2: BMW
+car3: Tesla
+ */
 }
+
+
+
+
