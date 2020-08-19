09cc61663b8d63661f15f5a2f45bde8fc4bb23db
Olga Andratova <orgaeva00@gmail.com>
2020-08-18 21:09:21 -0400

    day43 class

---
+++ src/day43_Static/staticBlock.java
@@ -0,0 +1,15 @@
+package day43_Static;
+
+public class staticBlock {
+
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+    static {
+        System.out.println("Static block");
+    }
+
+
+
+
+}
---
+++ src/day43_Static/staticBlock3.java
@@ -0,0 +1,17 @@
+package day43_Static;
+
+public class staticBlock3 {
+
+    static int a;
+    static int b;
+    static int c;
+
+    static {
+
+        a = 100;
+        b = 200;
+        c = 300;
+
+    }
+
+}
